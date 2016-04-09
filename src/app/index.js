8ba4c2f861369df55ac786259229c6ee521e9e4a
reiteristvan <reiteristvan@gmail.com>
2016-04-09 09:47:35 +0200

    Set up webpack and hot module loader

---
+++ src/app/index.js
@@ -0,0 +1,8 @@
+
+require('./styles/main.css');
+
+'use strict';
+
+var component = require('./component');
+
+document.body.appendChild(component());
---
+++ src/webpack.config.js
@@ -0,0 +1,52 @@
+
+const path = require('path');
+const merge = require('webpack-merge');
+const webpack = require('webpack');
+
+const TARGET = process.env.npm_lifecycle_event;
+const PATHS = {
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'build')  
+};
+
+const common = {
+  entry: {
+      app: PATHS.app
+  },
+  output: {
+      path: PATHS.build,
+      filename: 'bundle.js'
+  },
+  module: {
+      loaders: [
+          {
+              test: /\.css$/,
+              loaders: [ 'style', 'css' ],
+              include: PATHS.app
+          }
+      ]
+  }  
+};
+
+if(TARGET === 'start' || !TARGET){
+    module.exports = merge(common, {
+        devServer: {
+            contentBase: PATHS.build,
+            historyApiFallback: true,
+            hot: true,
+            inline: true,
+            progress: true,
+            stats: 'errors-only',
+            host: process.env.HOST,
+            port: process.env.PORT,
+            devtool: 'eval-source-map'
+        },
+        plugins: [
+            new webpack.HotModuleReplacementPlugin()
+        ]
+    });
+}
+
+if(TARGET === 'build'){
+    module.exports = merge(common, {});
+}
